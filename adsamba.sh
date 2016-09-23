28601e6a92ddd6dd41519842cc9f7bab7b6ce9a3
Mike Gregory <meikura@gmail.com>
2016-09-23 23:32:35 +0100

    Cleaned up whitespace and operators

--- adsamba.sh
+++ adsamba.sh
@@ -8,15 +8,13 @@ function createHome {
 	while read USER; do
 		# Check to see if they are in the ignore list.
 		PREFIX="$USER@foobar.com"
-		if ! grep -Fxq "$USER" $IGNORE
-		then
+		if ! grep -Fxq "$USER" $IGNORE; then
 
 		# Process the folder.
 		if [ ! -d "$MEDIA/$1/$USER" ]; then
 			# Initialise the user into the idmap.
 			wbinfo -i "$PREFIX"
-			if [ $? -eq 0 ]
-			then
+			if [ $? -eq 0 ]; then
 				wbUID=`id -u "$PREFIX"`
 				# Copy the template folder with permissions and set them.
 				cp -pr $MEDIA/$1/.template "$MEDIA/$1/$USER"
@@ -24,8 +22,7 @@ function createHome {
 				chown -R foobar\\"$USER" "$MEDIA/$1/$USER"
 				# Configure the account to have the specified quota.
 				setquota -u "$PREFIX" "$QUOTA" "$QUOTA" 0 0 -a
-				if [ $? -eq 0 ]
-				then
+				if [ $? -eq 0 ]; then
 					# All good.
 					grep -Fxq "$USER" $ADLIST && : || echo $USER>>$ADLIST
 					SUCCESS=`[[ "$SUCCESS" == "" ]] && echo "$USER" || echo "$SUCCESS, $USER"`
@@ -50,22 +47,22 @@ function createHome {
 		echo "   -- Processing $MEDIA/$1" >> $LOGFILE
 	fi
 
-	if [ ! "$SUCCESS" == "" ]; then
+	if [ "$SUCCESS" != "" ]; then
 		echo "      -- Successful Home Areas Created:" >> $LOGFILE
 		echo "         $SUCCESS" >> $LOGFILE
 	fi
 
-	if [ ! "$FAILPERM" == "" ]; then
+	if [ "$FAILPERM" != "" ]; then
 		echo "      -- FAIL: Could not set permissions:" >> $LOGFILE
 		echo "         $FAILPERM" >> $LOGFILE
 	fi
 
-	if [ ! "$FAILSID" == "" ]; then
+	if [ "$FAILSID" != "" ]; then
 		echo "      -- FAIL: Could not get user SID:" >> $LOGFILE
 		echo "         $FAILSID" >> $LOGFILE
 	fi
 
-	if [ ! "$FAILUSER" == "" ]; then
+	if [ "$FAILUSER" != "" ]; then
 		echo "      -- FAIL: Not a USER object:" >> $LOGFILE
 		echo "         $FAILUSER" >> $LOGFILE
 	fi
